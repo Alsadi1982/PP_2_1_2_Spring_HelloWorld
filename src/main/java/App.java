683285ee931f79d89b38151913f6760f527f6570
Alex Sadikov <sav1982@inbox.ru>
2022-12-04 01:33:46 +0300

    addaed Spring_HelloWorld solution

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,12 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Links bean and bean2 are equals: " + (bean == bean2));
+        System.out.println("Links cat1 and cat2 are equals: " + (cat1 == cat2));
         System.out.println(bean.getMessage());
     }
 }
